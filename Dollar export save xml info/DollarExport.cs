6dcf933c3b0d4383fd625d8e3e715b18c1b31f4a
Vyuga Snow <Wolf-snow00@mail.ru>
2023-10-03 23:31:48 +0300

    Добавьте файлы проекта.

---
+++ Dollar export save xml info/DollarExport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Net;
+using System.Xml;
+
+using Universal.Common.Collections;
+
+namespace Dollar_export_save_xml_info
+{
+    internal class XmlDownloader
+    {
+        public string URL { get; } = @"http://finance.i.ua/";
+        public string PathToFile { get; set; }
+
+        public XmlDownloader(string some, string path)
+        {
+            URL = URL.Replace("CODE", some);
+            PathToFile = path;
+        }
+
+        public MultiDictionary<string, string> GetDollarRate()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            MultiDictionary<string, string> bankRates = new MultiDictionary<string, string>();
+
+            xmlDocument.Load(PathToFile);
+
+            XmlNodeList xmlNodeList = xmlDocument.GetElementsByTagName("item");
+
+            foreach (XmlNode xmlNode in xmlNodeList)
+            {
+                XmlNode bankName = xmlNode.SelectSingleNode("bank");
+                XmlNode buyRate = xmlNode.SelectSingleNode("buyRate");
+                XmlNode sellRate = xmlNode.SelectSingleNode("sellRate");
+
+                bankRates[bankName.InnerText] = new List<string>() { buyRate.InnerText, sellRate.InnerText};
+            }
+            return bankRates;
+        }
+    }
+
+}
---
+++ Dollar export save xml info/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollar_export_save_xml_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order(new List<Product>() 
+                { 
+                new Product(157, "00154", "Banana"),
+                new Product(15000, "01073", "Fridge"),
+                new Product(5, "70180", "Rabbit")        
+                }
+            );
+
+            SaveOrderXml sOX = new SaveOrderXml(order, @"D:\Test\TestXml.xml");
+            sOX.SaveOrderToXml();
+        }
+    }
+}
