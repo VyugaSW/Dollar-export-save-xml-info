b5ffd421271325017646056974bf4b7e1d211b4f
Vyuga Snow <Wolf-snow00@mail.ru>
2023-10-05 16:50:13 +0300

    Ended the code

--- Dollar export save xml info/Program.cs
+++ Dollar export save xml info/Program.cs
@@ -10,16 +10,15 @@ namespace Dollar_export_save_xml_info
     {
         static void Main(string[] args)
         {
-            Order order = new Order(new List<Product>() 
-                { 
+            List<Product> order = new List<Product>()
+                {
                 new Product(157, "00154", "Banana"),
                 new Product(15000, "01073", "Fridge"),
-                new Product(5, "70180", "Rabbit")        
-                }
-            );
+                new Product(5, "70180", "Rabbit")
+                };
 
-            SaveOrderXml sOX = new SaveOrderXml(order, @"D:\Test\TestXml.xml");
-            sOX.SaveOrderToXml();
+            XmlWriterOrder sOX = new XmlWriterOrder(order, @"D:\Test\TestXml.xml", Encoding.Default);
+            sOX.SaveToXml();
         }
     }
 }
---
+++ Dollar export save xml info/XmlWriterOrder.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Net;
+using System.Xml;
+
+
+namespace Dollar_export_save_xml_info
+{
+
+    class Product : IComparable<Product>
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public int Price { get; }
+
+        public Product(int price, string id, string name)
+        {
+            Price = price;
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}\n{Name}\n{Price}";
+        }
+
+        public int CompareTo(Product obj)
+        {
+            return Price > obj.Price ? 1 : -1;
+            throw new Exception();
+        }
+    }
+
+
+    internal class XmlWriterOrder
+    {
+        string Path { get; }
+        Encoding Encoding { get; }
+        List<Product> Products { get; }
+
+        public XmlWriterOrder(List<Product> listOfProducts, string path, Encoding encod)
+        {
+            Products = listOfProducts;
+            Path = path;
+        }
+
+        public void SaveToXml()
+        {
+            using (Stream stream = new FileStream(Path, FileMode.Create))
+            {
+                XmlTextWriter writerXml = new XmlTextWriter(stream, Encoding);
+
+                writerXml.Formatting = Formatting.Indented; // For readability
+
+                writerXml.WriteStartElement("order"); // Write root element
+
+                for(int i = 0; i < Products.Count(); i++)
+                {
+                    writerXml.WriteStartElement("Product"); // Start product element
+                    writerXml.WriteAttributeString("number", $"{i+1}"); // Add attribute for element product
+
+                    // Write subelements
+                    writerXml.WriteElementString("Id", $"{ Products[i].Id}");
+                    writerXml.WriteElementString("Name", $"{ Products[i].Name}");
+                    writerXml.WriteElementString("Price", $"{ Products[i].Price}");
+
+                    writerXml.WriteEndElement(); // End product element
+                }
+
+                writerXml.WriteFullEndElement(); // End the root element
+                writerXml.Close();
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
